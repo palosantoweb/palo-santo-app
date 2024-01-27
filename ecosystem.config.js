5857763e1e838be84e552c0a9dace7e7c4a8e22f
Gonzalo Gomez <gonzalogomez@numetri.com>
2024-01-27 16:20:30 -0300

    Add ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [
+    {
+      script: 'npm start',
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: '149.50.136.81',
+      ref: 'origin/main',
+      repo: 'git@github.com:palosantoweb/palo-santo.git',
+      path: '/home/ubuntu',
+      'pre-deploy-local': '',
+      'post-deploy':
+        'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+      ssh_options: 'ForwardAgent=yes',
+      password: '5xpmvTU2rC6k',
+    },
+  },
+};
