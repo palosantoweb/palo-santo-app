f37d0615ab7c624634f70eccc7b71842aa493737
marianellagl <Marianella.gomezluna@gmail.com>
2024-02-03 21:44:46 -0300

    borrando archivos duplicados

--- app/utils/date.js
+++
@@ -1,9 +0,0 @@
-import moment from "moment";
-
-export const formatToDma = (date) => {
-  if (!date || isNaN(new Date(date)) || new Date(date) > new Date()) {
-    return '';
-}
-    const finalDate = new Date(date).toISOString();
-    return finalDate;
-  };
