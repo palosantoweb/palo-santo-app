35a737d5745a8aec3257810c8750b2af6b7ea787
GonzaloGDV <147004828+GonzaloNumetri@users.noreply.github.com>
2024-02-24 14:32:34 -0300

    Debbuging build

--- app/api/controllers/RoomController.js
+++ app/api/controllers/RoomController.js
@@ -1,31 +1,46 @@
-import { convertRoomHasClient, unconvertRoomHasClient } from "../converters/RoomHasClientConverter";
-import { RoomHasClient } from "../database/entities/RoomHasClient";
-import { RequestError } from "./error/ErrorHandler";
+import {
+  convertRoomHasClient,
+  unconvertRoomHasClient,
+} from '../converters/RoomHasClientConverter';
+import { RoomHasClient } from '../database/models/RoomHasClient';
+import { RequestError } from './error/ErrorHandler';
 
 export async function assignRoomToClient(rhc) {
-    // Valido datos
-    if (!rhc.client) {
-        throw new RequestError(new Error(), "Debe enviar los datos del cliente.", 409)
-    }
-    if (!rhc.room || !rhc.room.id) {
-        throw new RequestError(new Error(), "Debe enviar los datos de la habitacion.", 409)
-    }
+  // Valido datos
+  if (!rhc.client) {
+    throw new RequestError(
+      new Error(),
+      'Debe enviar los datos del cliente.',
+      409
+    );
+  }
+  if (!rhc.room || !rhc.room.id) {
+    throw new RequestError(
+      new Error(),
+      'Debe enviar los datos de la habitacion.',
+      409
+    );
+  }
 
-    // Corroboro que la habitacion no se encuentre ocupada
-    let dbRhc = await RoomHasClient.findOne({
-        where: {
-            status: "OCCUPIED",
-            room: {
-                id: rhc.room.id
-            }
-        }
-    })
-    if (dbRhc)
-        throw new RequestError(new Error(), "La habitacion se encuentra ocupada.", 409)
+  // Corroboro que la habitacion no se encuentre ocupada
+  let dbRhc = await RoomHasClient.findOne({
+    where: {
+      status: 'OCCUPIED',
+      room: {
+        id: rhc.room.id,
+      },
+    },
+  });
+  if (dbRhc)
+    throw new RequestError(
+      new Error(),
+      'La habitacion se encuentra ocupada.',
+      409
+    );
 
-    // Convierto y persisto la entidad
-    dbRhc = unconvertRoomHasClient(rhc)
-    dbRhc = await RoomHasClient.create(dbRhc)
+  // Convierto y persisto la entidad
+  dbRhc = unconvertRoomHasClient(rhc);
+  dbRhc = await RoomHasClient.create(dbRhc);
 
-    return convertRoomHasClient(dbRhc);
+  return convertRoomHasClient(dbRhc);
 }
