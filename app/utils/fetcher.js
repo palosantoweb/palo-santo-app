9fd0099ee8f8877f7ab25a263dea547759df76f5
Patricio Colella <patriciogabrielcolella@gmail.com>
2024-02-24 15:29:06 -0300

    fix routes

--- app/utils/fetcher.js
+++ app/utils/fetcher.js
@@ -1,6 +1,6 @@
 // import axios from "axios";
 
-const BASE_URL = process.env.NODE_ENV === 'production' ? "api/" : process.env.NEXT_PUBLIC_BASE_URL
+const BASE_URL = process.env.NODE_ENV === 'production' ? "api/routes" : process.env.NEXT_PUBLIC_BASE_URL
 
 // const instance = axios.create({
 //     baseURL: BASE_URL
