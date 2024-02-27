b36509243cc84c55f3a9c93aeaa464fcd0794ff6
marianellagl <Marianella.gomezluna@gmail.com>
2024-02-26 22:59:12 -0300

    fix

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: '149.50.136.81',
-        port: '9000',
-      },
-    ],
-  },
+  experimental:{
+    turbotrace:{
+      logAll:true
+    }
+  }
 };
