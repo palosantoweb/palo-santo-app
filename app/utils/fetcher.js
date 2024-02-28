59238f6147f5c29e3d829074ad9150e195cab591
marianellagl <Marianella.gomezluna@gmail.com>
2024-02-28 09:25:09 -0300

    fix env and routes

--- app/utils/fetcher.js
+++ app/utils/fetcher.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.NODE_ENV === 'production' ? "api/routes" : process.env.NEXT_PUBLIC_BASE_URL
+const BASE_URL = process.env.NODE_ENV === 'production' ? process.env.URL + "api/routes" : process.env.NEXT_PUBLIC_BASE_URL
 
 
 export const fetcher = async (url, options) => {
